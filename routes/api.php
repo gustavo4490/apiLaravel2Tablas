ae636dbd0c9f3c467cfae6fd3c7a749f3bb399ab
Andres <andresf150@gmail.com>
2024-03-30 14:38:33 -0600

    rutas v1

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('departments',DepartmentController::class);
-Route::resource('employee',EmployeeController::class);
-Route::get('employeeall',[EmployeeController::class,'all']);
-Route::get('employeesbydepartment',[EmployeeController::class,'EmployeesByDepartment']);
+Route::resource('v1/departments',DepartmentController::class);
+Route::resource('v1/employee',EmployeeController::class);
+Route::get('v1/employeeall',[EmployeeController::class,'all']);
+Route::get('v1/employeesbydepartment',[EmployeeController::class,'EmployeesByDepartment']);
 
