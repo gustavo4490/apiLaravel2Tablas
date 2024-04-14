1e49a6c68a4a18e0af9402e331ea201c0768208e
Andres <andresf150@gmail.com>
2024-04-13 20:15:52 -0600

    proteger rutas y agregar token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\Routing\Loader\AnnotationFileLoader;
 
 class AuthController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        // Definir reglas de validación
+        $rules = [
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8'
+        ];
+        $validator = \Validator::make($request->input(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ], 400);
+        }
+
+        // Crear usuario
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Usuario creado correctamente',
+            'token' => $user->createToken('API TOKEN')->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        // Definir reglas de validación
+        $rules = [
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string'
+        ];
+        $validator = \Validator::make($request->input(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ], 400);
+        }
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status' => false,
+                'errors' => ['no autorizado']
+            ], 401);
+        }
+        $user = User::where('email', $request->email)->first();
+        return response()->json([
+            'status' => true,
+            'message' => 'Usuario logueado correctamente',
+            'data' => $user,
+            'token' => $user->createToken('API TOKEN')->plainTextToken
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Sesión cerrada correctamente'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('v1/auth/register', [AuthController::class, 'create']);
+Route::post('v1/auth/login', [AuthController::class, 'login']);
+
+// proteger las rutas
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('v1/departments', DepartmentController::class);
+    Route::resource('v1/employee', EmployeeController::class);
+    Route::get('v1/employeeall', [EmployeeController::class, 'all']);
+    Route::get('v1/employeesbydepartment', [EmployeeController::class, 'EmployeesByDepartment']);
+    Route::get('v1/auth/logout', [AuthController::class, 'logout']);
+
 });
-
-Route::resource('v1/departments',DepartmentController::class);
-Route::resource('v1/employee',EmployeeController::class);
-Route::get('v1/employeeall',[EmployeeController::class,'all']);
-Route::get('v1/employeesbydepartment',[EmployeeController::class,'EmployeesByDepartment']);
-
